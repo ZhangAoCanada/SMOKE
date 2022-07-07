0ebb37d805607248ffffad7a7adcf35fc95a5d24
ZhangAoCanada <zhangaocanada@gmail.com>
2022-07-07 15:50:24 +0800

    running test

---
+++ inference_test.py
@@ -0,0 +1,55 @@
+import torch
+
+from smoke.config import cfg
+from smoke.data import make_data_loader
+from smoke.solver.build import (
+    make_optimizer,
+    make_lr_scheduler,
+)
+from smoke.utils.check_point import DetectronCheckpointer
+from smoke.engine import (
+    default_argument_parser,
+    default_setup,
+    launch,
+)
+from smoke.utils import comm
+from smoke.engine.trainer import do_train
+from smoke.modeling.detector import build_detection_model
+from smoke.engine.test_net import run_test
+
+
+def setup(args):
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+
+    model = build_detection_model(cfg)
+    device = torch.device(cfg.MODEL.DEVICE)
+    model.to(device)
+
+    checkpointer = DetectronCheckpointer(
+        cfg, model, save_dir=cfg.OUTPUT_DIR
+    )
+    ckpt = cfg.MODEL.WEIGHT if args.ckpt is None else args.ckpt
+    _ = checkpointer.load(ckpt, use_latest=args.ckpt is None)
+    return run_test(cfg, model)
+
+
+if __name__ == '__main__':
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
